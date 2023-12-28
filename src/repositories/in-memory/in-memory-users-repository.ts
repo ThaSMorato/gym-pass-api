2296fa71233bc6d2ead0625fae7055f83d8a64eb
ThaSMorato <tha.s.morato@gmail.com>
2023-12-28 00:38:41 -0300

    refact: using randomUUID for id

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,6 +1,6 @@
 import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
-import { randomInt } from 'crypto'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
@@ -17,7 +17,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: `a_user_id_${randomInt(100)}`,
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
