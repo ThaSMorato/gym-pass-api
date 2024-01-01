b55f2f1e57bf44096484c1fbe9148009c32ee883
ThaSMorato <tha.s.morato@gmail.com>
2024-01-01 18:52:27 -0300

    feat: me route and jwt verify

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
+import { verifyJwt } from './middlewares/verifyJwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // Autenticated
+  app.addHook('onRequest', verifyJwt)
+  app.get('/me', profile)
 }
